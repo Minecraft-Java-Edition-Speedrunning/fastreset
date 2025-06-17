3d43f495fb228368dfce09ce2f1b60cc5cf6184e
tildejustin <spacepepper3.14@gmail.com>
2025-06-17 13:11:15 -0400

    port to 1.21.6

--- src/main/java/fast_reset/client/client/SaveWorldScreen.java
+++ src/main/java/fast_reset/client/client/SaveWorldScreen.java
@@ -1,5 +1,6 @@
 package fast_reset.client.client;
 
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
@@ -11,11 +12,11 @@ public class SaveWorldScreen extends Screen {
         super(Text.translatable("menu.savingLevel"));
     }
 
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta){
-        renderBackground(matrices);
+    public void render(DrawContext ctx, int mouseX, int mouseY, float delta){
+        renderBackground(ctx, mouseX, mouseY, delta);
 
-        super.render(matrices, mouseX, mouseY, delta);
+        super.render(ctx, mouseX, mouseY, delta);
 
-        drawCenteredTextWithShadow(matrices, this.textRenderer, "still saving the last world", this.width / 2, 70, Color.white.getRGB());
+        ctx.drawCenteredTextWithShadow(this.textRenderer, "still saving the last world", this.width / 2, 70, Color.white.getRGB());
     }
 }
--- src/main/java/fast_reset/client/mixin/GameMenuMixin.java
+++ src/main/java/fast_reset/client/mixin/GameMenuMixin.java
@@ -3,9 +3,11 @@ package fast_reset.client.mixin;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import fast_reset.client.Client;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.*;
 import net.minecraft.client.gui.screen.*;
 import net.minecraft.client.gui.widget.*;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.*;
@@ -18,20 +20,21 @@ public abstract class GameMenuMixin extends Screen {
         super(title);
     }
 
-    @Shadow
-    protected abstract void disconnect();
-
     @Unique
     private static final int bottomRightWidth = 102;
 
     @Shadow
     private @Nullable ButtonWidget exitButton;
 
+    @Shadow
+    public static void disconnect(MinecraftClient client, Text disconnectReason) {
+    }
+
     @Redirect(method = "initWidgets", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget$Adder;add(Lnet/minecraft/client/gui/widget/Widget;I)Lnet/minecraft/client/gui/widget/Widget;"))
     private <T extends Widget> T addButtons(GridWidget.Adder instance, T widget, int occupiedColumns, @Share("saveButton") LocalRef<ButtonWidget.Builder> saveButtonStore, @Share("quitButton") LocalRef<T> quitButton) {
         final ButtonWidget.Builder saveButton = ButtonWidget.builder(Text.translatable("menu.quitWorld"), (buttonWidgetX) -> {
             Client.saveOnQuit = false;
-            this.disconnect();
+            disconnect(this.client, ClientWorld.QUITTING_MULTIPLAYER_TEXT);
             Client.saveOnQuit = true;
         });
         if (Client.buttonLocation == 2) {
