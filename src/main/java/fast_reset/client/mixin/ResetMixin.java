79591ff9176241a6575d7c319861ab9b6d577d0d
tildejustin <spacepepper3.14@gmail.com>
2025-08-31 19:38:06 -0400

    1.5.1: remove async world closing

--- src/main/java/fast_reset/client/mixin/ResetMixin.java
+++ src/main/java/fast_reset/client/mixin/ResetMixin.java
@@ -4,22 +4,16 @@ import fast_reset.client.Client;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.level.storage.LevelStorage;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.io.IOException;
 import java.util.Iterator;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 @Mixin(MinecraftServer.class)
 public class ResetMixin {
-    @Shadow @Final protected LevelStorage.Session session;
-
     // kill save on the shutdown
     @Redirect(method = "shutdown", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z", ordinal = 0))
     private boolean getWorldsInjectOne(Iterator<ServerWorld> iterator){
@@ -29,29 +23,6 @@ public class ResetMixin {
         while(iterator.hasNext()){
             iterator.next().savingDisabled = true;
         }
-        return false;
-    }
-
-    @Redirect(method = "shutdown", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z", ordinal = 2))
-    private boolean getWorldsInjectTwo(Iterator<ServerWorld> iterator){
-        if(Client.saveOnQuit){
-            return iterator.hasNext();
-        }
-
-        new Thread(() -> {
-            synchronized(Client.saveLock){
-                while(iterator.hasNext()){
-                    ServerWorld world = iterator.next();
-                    if (world != null) {
-                        try {
-                            world.close();
-                        } catch (IOException ignored) {
-
-                        }
-                    }
-                }
-            }
-        }).start();
         return false;
     }
 
